0cc22884546b52e5a8a12a10b217e6ce8c9a011a
matheusfugazi <matheusbt2024@gmail.com>
2025-03-20 03:00:58 +0000

    terminei o codigo do jogo de cartas Super Trunfo, agora é só testar e ver se funciona

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,35 +3,64 @@
 int main(){
 
 printf("desafio cartas!\n");
+printf("Carta A01!\n");
 
-char Estado[10];
-char Codigo [5];
-char Cidade [20];
-int Populacao;
-float Area;
-float Pib;
-int pontosturisticos;
+char Estado1[10] ,Codigo1[5] , Cidade1[20];
+int Populacao1 ,Pontosturisticos1;
+float Area1, Pib1;
 
 printf("digite seu estado: \n");
-scanf("%s", &Estado);
+scanf("%s", &Estado1);
 
 printf("Codigo: \n");
-scanf("%s", &Codigo);
+scanf("%s", &Codigo1);
 
 printf("Nome Da Cidade: \n");
-scanf("%s", &Cidade);
+scanf("%s", &Cidade1);
 
 printf("Total De Habitantes: \n" );
-scanf ("%d", &Populacao);
+scanf ("%d", &Populacao1);
 
 printf("Area: \n");
-scanf("%d", &Area);
+scanf("%d", &Area1);
 
 printf("Pib: \n");
-scanf("%d", &Pib);
+scanf("%d", &Pib1);
 
 printf("Quantidade De Pontos Turisticos: \n");
-scanf("%d", &pontosturisticos);
+scanf("%d", &Pontosturisticos1);
+
+
+printf("carta A02\n");
+
+char Estado2[10];
+char Codigo2 [5];
+char Cidade2 [20];
+int Populacao2;
+float Area2;
+float Pib2;
+int pontosturisticos2;
+
+printf("digite seu estado: \n");
+scanf("%s", &Estado2);
+
+printf("Codigo: \n");
+scanf("%s", &Codigo2);
+
+printf("Nome Da Cidade: \n");
+scanf("%s", &Cidade2);
+
+printf("Total De Habitantes: \n" );
+scanf ("%d", &Populacao2);
+
+printf("Area: \n");
+scanf("%d", &Area2);
+
+printf("Pib: \n");
+scanf("%d", &Pib2);
+
+printf("Quantidade De Pontos Turisticos: \n");
+scanf("%d", &pontosturisticos2);
 
 return 0;
 
